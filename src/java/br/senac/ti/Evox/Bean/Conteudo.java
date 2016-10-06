8aaa1327df2b976685b63790c66b98136a2c224e
Naruto(admin) <Naruto(admin)@192.168.2.16>
2016-10-06 18:05:03 -0300

    criado a classe sessão

---
+++ src/java/br/senac/ti/Evox/Bean/Conteudo.java
@@ -0,0 +1,105 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.senac.ti.Evox.Bean;
+
+/**
+ *
+ * @author Naruto(admin)
+ */
+public class Conteudo {
+    
+    private int id;
+    private String titulo;
+    private String descricao;
+    private String texto;
+    private boolean ativo;
+    private int sessao_id;
+
+    /**
+     * @return the id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the titulo
+     */
+    public String getTitulo() {
+        return titulo;
+    }
+
+    /**
+     * @param titulo the titulo to set
+     */
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    /**
+     * @return the descricao
+     */
+    public String getDescricao() {
+        return descricao;
+    }
+
+    /**
+     * @param descricao the descricao to set
+     */
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    /**
+     * @return the texto
+     */
+    public String getTexto() {
+        return texto;
+    }
+
+    /**
+     * @param texto the texto to set
+     */
+    public void setTexto(String texto) {
+        this.texto = texto;
+    }
+
+    /**
+     * @return the ativo
+     */
+    public boolean isAtivo() {
+        return ativo;
+    }
+
+    /**
+     * @param ativo the ativo to set
+     */
+    public void setAtivo(boolean ativo) {
+        this.ativo = ativo;
+    }
+
+    /**
+     * @return the sessao_id
+     */
+    public int getSessao_id() {
+        return sessao_id;
+    }
+
+    /**
+     * @param sessao_id the sessao_id to set
+     */
+    public void setSessao_id(int sessao_id) {
+        this.sessao_id = sessao_id;
+    }
+    
+}
